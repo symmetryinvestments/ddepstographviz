820aa6d23d4435cdd414c497583a5a7f6d6c8a09
Robert Schadek <rschadek@symmetryinvestments.com>
2019-10-10 10:55:38 +0100

    a bit of coloring

--- source/app.d
+++ source/app.d
@@ -1,10 +1,11 @@
 import std.stdio;
 
-import std.array : array, empty, front, popBack, replace;
+import std.array : array, empty, front, popBack, popFront, replace;
 import std.algorithm.searching : find, startsWith;
 import std.algorithm.iteration : each, filter, map, joiner, splitter;
 import std.algorithm.sorting : sort;
 import std.algorithm.iteration : uniq;
+import std.exception : enforce;
 import std.typecons : nullable, Nullable;
 import std.format : format, formattedWrite;
 import std.range : tee;
@@ -127,6 +128,7 @@ struct Edge {
 
 class Node {
 	string name;
+	string color;
 	Edge[] connections;
 	Node[] subNodes;
 	
@@ -137,7 +139,7 @@ class Node {
  	}
 
 	@property bool isCluster() const {
-		return this.subNodes.empty;
+		return !this.subNodes.empty;
 	}
 
 	void add(Edge con) {
@@ -185,12 +187,14 @@ void print(Node node, int depth) {
 
 void toDOT(Out)(auto ref Out o, Node[] nodes) {
 	formattedWrite(o, "digraph Deps {\n");	
+	formattedWrite(o, "\tcompound=true;\n");
+ 
 	foreach(n; nodes) {
 		toDOT(o, n, [n.name]);
 	}
 	formattedWrite(o, "\n\n");	
 	foreach(n; nodes) {
-		toDOTEdges(o, n, [n.name]);
+		toDOTEdges(o, n, [n.name], nodes);
 	}
 	formattedWrite(o, "}\n");	
 }
@@ -200,7 +204,10 @@ void toDOT(Out)(auto ref Out o, Node node, string[] stack) {
 	if(node.isCluster) {
 		string n = node.name.replace('.', '_');
 		n = n == "graph" ? "_graph" : n;
-		formattedWrite(o, "subgraph cluster%-(%s_%) {\n", stack);
+		formattedWrite(o, "subgraph cluster%-(%s_%) {\n", 
+				stack.map!(it => it == "graph" ?  "_graph" : it));
+		indent(o, stack.length + 1);
+		formattedWrite(o, "compound=true;\n");
 		indent(o, stack.length + 1);
 		formattedWrite(o, "rankdir=\"%s\";\n", 
 				stack.length % 2 == 0 ? "TB" : "LR");
@@ -208,6 +215,8 @@ void toDOT(Out)(auto ref Out o, Node node, string[] stack) {
 		formattedWrite(o, "label=\"%s\";\n", node.name);
 		indent(o, stack.length + 1);
 		formattedWrite(o, "labeljust=l;\n");
+		indent(o, stack.length + 1);
+		formattedWrite(o, "color=\"%s\";\n", node.color);
 		foreach(sn; node.subNodes) {
 			stack ~= sn.name;
 			toDOT(o, sn, stack);
@@ -216,14 +225,18 @@ void toDOT(Out)(auto ref Out o, Node node, string[] stack) {
 		indent(o, stack.length);
 		formattedWrite(o, "}\n");
 	} else {
-		formattedWrite(o, "%-(%s_%) [label=\"%s\"];\n", 
+		formattedWrite(o, "%-(%s_%) [label=\"%s\",color=\"%s\"];\n", 
 				stack.map!(it => it == "graph" ?  "_graph" : it),
-				node.name);
+				node.name, node.color);
 	}
 }
 
-void toDOTEdges(Out)(auto ref Out o, Node node, string[] stack) {
+void toDOTEdges(Out)(auto ref Out o, Node node, string[] stack, 
+		const(Node)[] graph) 
+{
 	foreach(e; node.connections.filter!(con => !con.to.empty)) {
+		const(Node) toNode = graph.findNode(e.to);
+		enforce(toNode, format("Couldn't find '%s' in the graph", e.to));
 		string to = format("%s", e.to
 			.splitter(".")
 			.map!(it => it == "graph" ?  "_graph" : it)
@@ -232,17 +245,46 @@ void toDOTEdges(Out)(auto ref Out o, Node node, string[] stack) {
 		string from = format("%-(%s_%)", 
 				stack.map!(it => it == "graph" ?  "_graph" : it));
 		from = node.subNodes.empty ? from : "cluster" ~ from;
-		if(from.startsWith("cluster")) {
+
+		// No internal edges
+		auto f = node.subNodes
+				.find!( (const(Node) n, const(Node) o) => n.name == o.name)(toNode);
+
+		if(!f.empty) {
 			continue;
 		}
-		//from = from == "graph" ? "_graph" : from;
-		formattedWrite(o, "\t%s -> %s;\n", from, to);
+
+		const bool atLeastOnCluster = node.isCluster || toNode.isCluster;
+		if(atLeastOnCluster) {
+			formattedWrite(o, "\t%s -> %s [%s %s, color=\"%s\"];\n", from, to,
+						node.isCluster ? format("ltail=%s", from) : "",
+						toNode.isCluster ? format("lhead=%s", from) : "",
+						node.color
+					);
+		} else {
+			formattedWrite(o, "\t%s -> %s [color=\"%s\"];\n", from, to,
+					node.color);
+		}
 	}
 	foreach(sn; node.subNodes) {
 		stack ~= sn.name;
-		toDOTEdges(o, sn, stack);
+		toDOTEdges(o, sn, stack, graph);
 		stack.popBack();
 	}
+}
+
+const(Node) findNode(const(Node)[] graph, string to) {
+	return findNodeImpl(graph, to.split("."));
+}
+
+const(Node) findNodeImpl(const(Node)[] graph, string[] to) {
+	auto f = graph.find!( (const(Node) n, string name) => n.name == name)(to.front);
+	to.popFront();
+	return to.empty && !f.empty 
+		? f.front 
+		: !to.empty && !f.empty
+			? findNodeImpl(f.front.subNodes, to)
+			: null;
 }
 
 Node[] linesToNodes(Line[] lines) {
@@ -274,6 +316,7 @@ void insertInto(Line line, ref Node[] nodes) {
 		return;
 	} else if(n.empty || line.from.length == 1) {
 		nodes ~= new Node(line);
+		nodes.back.color = nextColor();
 		//writeln(nodes.back);
 		return;
 	} else {
@@ -285,4 +328,43 @@ void insertInto(Line line, ref Node[] nodes) {
 		return;
 	}
 	assert(false, format("%s", line));
+}
+
+immutable(string[]) colors = [
+	"cadetblue2", "darkolivegreen", "darkviolet", "darkseagreen1",
+	"floralwhite", "darkgoldenrod", "aquamarine1", "black", "chartreuse3",
+	"gold3", "darkseagreen2", "cyan2", "bisque4", "antiquewhite",
+	"darkseagreen", "forestgreen", "coral4", "darkgoldenrod2", "dodgerblue3",
+	"darkorange2", "antiquewhite3", "chartreuse2", "goldenrod1", "goldenrod4",
+	"dodgerblue", "darkorange3", "chocolate3", "burlywood3", "crimson",
+	"antiquewhite4", "darkslategrey", "darkslategray1", "firebrick1",
+	"chartreuse", "dimgrey", "darkgreen", "darkgoldenrod3", "aquamarine2",
+	"bisque3", "cyan4", "bisque2", "coral", "goldenrod", "burlywood2",
+	"deeppink1", "deepskyblue1", "deepskyblue3", "bisque1", "cadetblue",
+	"blue3", "dodgerblue1", "deeppink", "coral1", "darkorchid3",
+	"antiquewhite1", "bisque", "firebrick3", "blue2", "gold", "blueviolet",
+	"goldenrod3", "burlywood4", "cyan3", "firebrick2", "firebrick",
+	"darkorange", "beige", "darkseagreen3", "darkolivegreen1", "gold4",
+	"firebrick4", "burlywood", "darkslategray3", "cornflowerblue",
+	"blanchedalmond", "brown1", "aquamarine", "chocolate", "darkorchid4",
+	"darkslategray2", "chocolate4", "deepskyblue", "azure", "gold2",
+	"aquamarine3", "darkseagreen4", "darkolivegreen2", "cornsilk1", "brown3",
+	"chartreuse4", "azure1", "darkkhaki", "goldenrod2", "darkorchid",
+	"deepskyblue2", "deepskyblue4", "darksalmon", "deeppink2", "gold1",
+	"cornsilk", "burlywood1", "blue1", "darkgoldenrod4", "darkturquoise",
+	"cyan1", "chartreuse1", "darkorange4", "cyan", "darkolivegreen4",
+	"blue4", "darkolivegreen3", "cornsilk4", "aquamarine4", "brown2", "coral3",
+	"darkslateblue", "aliceblue", "cadetblue3", "cadetblue1", "brown",
+	"gainsboro", "antiquewhite2", "coral2", "chocolate2", "darkslategray4",
+	"azure2", "darkorchid2", "deeppink4", "darkslategray", "blue", "cornsilk2",
+	"ghostwhite", "azure3", "darkgoldenrod1", "darkorange1", "azure4",
+	"chocolate1", "darkorchid1", "dodgerblue2", "dimgray", "dodgerblue4",
+	"cornsilk3", "cadetblue4", "brown4", "deeppink3"
+];
+
+string nextColor() {
+	static size_t idx;
+	string ret = colors[idx];
+	idx = (idx + 1) % colors.length;
+	return ret;
 }
